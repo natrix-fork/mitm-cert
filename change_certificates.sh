157b3f50622c5f194a92168666471878b1df85bd
natrix-fork <cqrdev@gmail.com>
2019-08-22 16:09:46 +0300

    Create change_certificates.sh

---
+++ change_certificates.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+set -e
+CURRENT_FOLDER=`pwd`
+# this file have certificate + key
+PATH_TO_FILE="${CURRENT_FOLDER}/natrix-certificates/mitmproxy.pem"
+EXTENSION="${PATH_TO_FILE##*.}"
+
+TEMP=`cat ${PATH_TO_FILE}`
+
+# find
+#	in path "/"
+#	-type f: files
+#	-name "*.pem": with name, end to ".pem"
+#	-exec sh change_file.sh {} "${TEMP}" \; : and execute command [sh change_file.sh {} "${TEMP}"]
+#		where:
+#			- "{}" is path to file
+#			- "change_file.sh" script with further processing. Having problems with appending to file via command itself.
+
+ACTION="replace_content_of_file"
+find / -type f -name "*.${EXTENSION}" -exec sh "${CURRENT_FOLDER}/natrix-replace/${ACTION}.sh" {} "${TEMP}" \;
