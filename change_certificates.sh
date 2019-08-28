d00751749088eab53606aae42b8d02a7c13fc1b3
natrix-fork <cqrdev@gmail.com>
2019-08-28 13:46:45 +0300

    Update change_certificates.sh

--- change_certificates.sh
+++ change_certificates.sh
@@ -2,10 +2,17 @@
 set -e
 #CURRENT_FOLDER=`pwd`
 # this file have certificate + key
+PATH_TO_FILE="mycert.crt"
+EXTENSION="${PATH_TO_FILE##*.}"
+TEMP=`cat ${PATH_TO_FILE}`
+ACTION="replace_content_of_file"
+find / -type f -name "*.${EXTENSION}" -exec sh "${ACTION}.sh" {} "${TEMP}" \;
+
 PATH_TO_FILE="mycert.pem"
 EXTENSION="${PATH_TO_FILE##*.}"
-
 TEMP=`cat ${PATH_TO_FILE}`
+ACTION="replace_content_of_file"
+find / -type f -name "*.${EXTENSION}" -exec sh "${ACTION}.sh" {} "${TEMP}" \;
 
 # find
 #	in path "/"
@@ -15,6 +22,3 @@ TEMP=`cat ${PATH_TO_FILE}`
 #		where:
 #			- "{}" is path to file
 #			- "change_file.sh" script with further processing. Having problems with appending to file via command itself.
-
-ACTION="replace_content_of_file"
-find / -type f -name "*.${EXTENSION}" -exec sh "${ACTION}.sh" {} "${TEMP}" \;
