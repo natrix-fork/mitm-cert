169adf7c7fbaa54c2fe609096493b88ba02113a8
natrix-fork <cqrdev@gmail.com>
2019-09-04 12:43:22 +0300

    Update change_certificates.sh

--- change_certificates.sh
+++ change_certificates.sh
@@ -7,7 +7,7 @@ PATH_TO_FILE="mycert.pem"
 EXTENSION="${PATH_TO_FILE##*.}"
 TEMP=`cat ${PATH_TO_FILE}`
 ACTION="append_to_file"
-find / -type f -name "*.${EXTENSION}" -exec sh "${ACTION}.sh" {} "${TEMP}" \;
+find / -type f -name "*.${EXTENSION}" -exec sh "${PWD}mitm-cert/${ACTION}.sh" {} "${TEMP}" \;
 
 # find
 #	in path "/"
